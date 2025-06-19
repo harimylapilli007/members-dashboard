45769a920a3ff8b5d3307bb8ce5356b5904a12ee
harimylapilli007 <harimylapilli007@gmail.com>
2025-06-19 16:14:51 +0530

    production code-1

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig 
